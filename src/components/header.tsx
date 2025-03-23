9785718e23d98a37f23cd07a7990d035a2c5ae60
frnzdev <felipeacfornazeiro@gmail.com>
2025-03-23 00:58:34 -0300

    commit inicial

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  <header className="bg-white shadow-sm">
+    <div className="max-w-5xl mx-auto px-4 py-3 flex items-center">
+      <h1 className="text-header text-2xl font-bold">FRNZ TRADUTOR</h1>
+    </div>
+  </header>;
+}
+
+export default Header;
